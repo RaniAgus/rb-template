eff53fbfd4a14112db999bb294955e60ef3c41d6
Agustin Ranieri <aguseranieri@gmail.com>
2023-09-29 19:32:38 -0300

    add matcher example

--- spec/golondrina_spec.rb
+++ spec/golondrina_spec.rb
@@ -4,12 +4,12 @@ describe 'golondrina' do
   it 'al volar se reduce su energia' do
     pepita.volar(10)
  
-    expect(pepita.energia).to eq(70)
+    expect(pepita).to have_energia(70)
   end
 
   it 'al comer aumenta su energia' do
     pepita.comer(10)
 
-    expect(pepita.energia).to eq(120)
+    expect(pepita).to have_energia(120)
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,8 @@
 require 'rspec'
 require_relative '../lib/golondrina'
+
+RSpec::Matchers.define :have_energia do |expected|
+  match do |actual|
+    actual.energia == expected
+  end
+end
