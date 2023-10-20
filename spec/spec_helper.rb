db126967b7f7b08a9b96298e9a4b3c244368f493
Agustin Ranieri <aguseranieri@gmail.com>
2023-10-19 21:13:35 -0300

    add failure message example

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,4 +5,8 @@ RSpec::Matchers.define :have_energia do |expected|
   match do |actual|
     actual.energia == expected
   end
+
+  failure_message do |actual|
+    "expected that:\n#{actual.energia}\n\nwould be equal to:\n#{expected}"
+  end
 end
